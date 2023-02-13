eed1987f0a7a1dcee030116ea1a0d55db9729475
Eugen Fazekas <skybolt83@gmail.com>
2023-02-13 23:19:13 +0200

    added binary classification and convolutional neural network example

---
+++ Building_Convolutional_Neural_Networks/src/main/java/com/springdl4j/springdl4j/BuildingConvolutionalNeuralNetworksApplication.java
@@ -0,0 +1,14 @@
+package com.springdl4j.springdl4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//Run this only after building project 'cookbookapp-cnn' and jar file is created.
+@SpringBootApplication
+public class BuildingConvolutionalNeuralNetworksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingConvolutionalNeuralNetworksApplication.class, args);
+	}
+
+}
---
+++ Building_Deep_Neural_Networks_for_Binary_classification/src/main/java/com/springdl4j/springdl4j/BuildingDeepNeuralNetworksForBinaryClassificationApplication.java
@@ -0,0 +1,13 @@
+package com.springdl4j.springdl4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildingDeepNeuralNetworksForBinaryClassificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildingDeepNeuralNetworksForBinaryClassificationApplication.class, args);
+	}
+
+}
